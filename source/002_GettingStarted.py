bc9dbdadd4e4786ac64d589ca6dc1c10e52bbb23
Babu Reddy <babureddyh@hotmail.com>
2019-09-28 17:31:17 +0530

    002: Getting Started with FastAPI.

---
+++ source/002_GettingStarted.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI(debug = True)
+
+
+# A simple hello world api. It uses the get method. / indicates the root, however this doesn't have any reason why the method is named root.
+# The method name can be anything.
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host = "127.0.0.1", port = 8000)
