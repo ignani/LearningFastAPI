54bf5e84cb8e4f29ecbdd3a33cbd2759ee53fa29
Babu Reddy <babureddyh@hotmail.com>
2020-01-04 21:51:20 +0530

    008: Numeric Validation with FastAPI.

--- source/008_NumericValidation.py
+++ source/008_NumericValidation.py
@@ -9,24 +9,21 @@ async def read_items(item_id: str = Query(..., min_length = 2, max_length = 10, 
     results.update({"New Item": item_id})
     return {"item": item_id}
 
+
+# Path Parameter Validation
 # @app.get("/items/{item_id}")
-# async def read_items(item: str, item_id: str = Path(...,description = "Item id")):
-#     return {"item_id": item_id, "item": item}
-
-
-# Basic Number Validation
+# async def get_items(item_id: str = Path(..., min_length = 2, max_length = 10, regex="^Item\d{1,6}")):
+#     return {"item_id": item_id}
+#
+#
+# Path & Query Parameters
 # @app.get("/items/{item_id}")
-# async def read_items(item: str,
-#                      item_id: float = Path(...,description = "Item id")):
-#     return {"item_id": item_id, "item": item}
-
-
-# Max & Minimum number validation
+# async def get_items(item: str = Query(None), item_id: str = Path(..., min_length = 2, max_length = 10, regex="^Item\d{1,6}")):
+#     return {"item_id": item_id, "item" : item}
+#
 @app.get("/items/{item_id}")
-async def read_items(item: int = Query(None, gt = 1, le = 100),
-                     item_id: float = Path(...,description = "Item id", ge = 1, le = 10,)):
-    return {"item_id": item_id, "item": item}
-
+async def get_items(item_id: float = Path(..., le = 10, ge = 2, alias = "Item-id", title = "Item Id")):
+    return {"item_id": item_id}
 
 if __name__ == "__main__":
     uvicorn.run(app, host = "127.0.0.1", port = 8000)
